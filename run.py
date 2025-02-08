9fc3b021cc78776ef065f416240de4949cfa1455
oscarka <ka-0724@hotmail.com>
2025-02-08 12:43:07 +0800

    更新部署配置和清理测试文件

--- run.py
+++ run.py
@@ -1,3 +1,4 @@
+import os
 from app import create_app
 from app.utils.logging import init_logging
 
@@ -5,4 +6,4 @@ app = create_app()
 init_logging(app)
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5001)
+    app.run(host='0.0.0.0', port=5001, debug=True)
