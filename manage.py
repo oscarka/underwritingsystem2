3b7ca87db5622a3f7a808ef9c5c5661a5587ee94
oscarka <ka-0724@hotmail.com>
2025-02-08 13:55:14 +0800

    fix: 移除 flask-script，使用 flask.cli 替代

--- manage.py
+++ manage.py
@@ -1,11 +1,10 @@
-from flask_migrate import Migrate, MigrateCommand
-from flask_script import Manager
+from flask.cli import FlaskGroup
 from app import create_app, db
+from flask_migrate import Migrate
 
 app = create_app()
 migrate = Migrate(app, db)
-manager = Manager(app)
-manager.add_command('db', MigrateCommand)
+cli = FlaskGroup(create_app=create_app)
 
 if __name__ == '__main__':
-    manager.run() 
+    cli() 
